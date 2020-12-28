0a95f01b5a5f4b93e903f3b1527df545cbdb1029
Karan Chawla <karan.chawla.19@ucl.ac.uk>
2020-12-28 17:46:19 +0000

    remove old project folder

--- ActivityLeague3/respondent/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RespondentConfig(AppConfig):
-    name = 'respondent'
--- ActivityLeague3/surveyor/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Group, GroupSurveyor, Question, Surveyor, Task
-
-admin.site.register(Surveyor)
-admin.site.register(Task)
-admin.site.register(Group)
-admin.site.register(GroupSurveyor)
-admin.site.register(Question)
