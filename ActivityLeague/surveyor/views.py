a608f9d0282ff5656069454bb657e82622fa024c
jweig0ld <jamie.weigold.19@ucl.ac.uk>
2021-01-04 19:30:36 +0100

    Restored existing ability to create new tasks

--- ActivityLeague/surveyor/views.py
+++ ActivityLeague/surveyor/views.py
@@ -19,12 +19,10 @@ def task_overview(request, pk):
     # return render(request, 'task_overview.html')
 
 def new_task(request, pk):
-    # Create new task with questions
-
-    print("We reached here!")
     user = get_object_or_404(Surveyor, pk=pk)
     group_surveyors = GroupSurveyor.objects.filter(surveyor_id=pk)
     groups = []
+
     for gr in group_surveyors:
         groups.append(Group.objects.get(pk=gr.id))
 
@@ -36,20 +34,21 @@ def new_task(request, pk):
         formset = QuestionFormset(request.POST)
         # if form.is_valid() and formset.is_valid():
         print("Valid form" if form.is_valid() else "Invalid form")
+        print(form.errors)
         if True:
             task = form.save(commit=False)
             task.save()
 
             for question_form in formset:
                 question = question_form.save(commit=False)
-                question.task_id = task
+                question.task = task
                 
                 # TODO: Add the other question fields
                 question.save() # BUG
 
             task.title = form.cleaned_data['title']
             task.due_date = form.cleaned_data['due_date']
-            task.group_id = Group.objects.get(name=form.cleaned_data['group_id'])
+            task.group = Group.objects.get(name=form.cleaned_data['group'])
             print(task.title)
             # task.save()
             # task = Task.objects.create(
