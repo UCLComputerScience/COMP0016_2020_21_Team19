72d4f09f45cffeb504d4afcd357de4aea73e1b72
jweig0ld <jamie.weigold.19@ucl.ac.uk>
2021-01-29 20:18:05 +0100

    Signing up and signing in works

--- ActivityLeague/authentication/adapter.py
+++ ActivityLeague/authentication/adapter.py
@@ -6,7 +6,15 @@ from respondent.models import Respondent
 class MyAccountAdapter(DefaultAccountAdapter):
 
     def get_login_redirect_url(self, request):
+        if Surveyor.objects.filter(user=request.user):
+            path = "/surveyor@1"
+        elif Respondent.objects.filter(user=request.user):
+            path = "/respondent@1"
+        else:
+            raise Exception("User type not expected.")
+        return path.format(username=request.user.username)
 
+    def get_signup_redirect_url(self, request):
         if Surveyor.objects.filter(user=request.user):
             path = "/surveyor@1"
         elif Respondent.objects.filter(user=request.user):
