ca6acac48e78c61dff214cc32220e449b724d39a
Karan Chawla <karan.chawla.19@ucl.ac.uk>
2021-03-05 19:19:38 +0000

    Handle invited SSO sign up

--- authentication/signals.py
+++ authentication/signals.py
@@ -3,6 +3,8 @@ from allauth.socialaccount.signals import pre_social_login
 from django.dispatch import receiver
 
 from core.models import UserInvitation
+from surveyor.models import Surveyor
+from respondent.models import Respondent
 
 
 # @receiver(user_signed_up)
@@ -15,5 +17,31 @@ from core.models import UserInvitation
 
 @receiver(pre_social_login)
 def pre_social_login(request, sociallogin, **kwargs):
-    email = sociallogin.account.user.email
-    # TODO
+    user = sociallogin.account.user
+    # New user signing up
+    if not (Surveyor.objects.filter(user=user).exists() or Respondent.objects.filter(user=user).exists()):
+        if UserInvitation.objects.filter(email=user.email).exists():
+            invite =  UserInvitation.objects.get(email=user.email)
+            if invite.is_respondent:
+                group = invite.group
+                respondent = Respondent(
+                    user=user,
+                    firstname=user.first_name,
+                    surname=user.last_name
+                )
+                respondent.save()
+                group_respondent = GroupRespondent(
+                    group=group,
+                    respondent=respondent
+                )
+                group_respondent.save()
+            else:
+                organisation = invite.organisation
+                surveyor = Surveyor(
+                    user=user,
+                    firstname=user.first_name,
+                    surname=user.last_name,
+                    organisation=organisation
+                )
+                surveyor.save()
+    # TODO: need to handle case of organisation admin signing up with social account
