5e4ea13ac776fbc8f17b47937900083959ce5bc7
Karan Chawla <karan.chawla.19@ucl.ac.uk>
2021-03-15 00:17:22 +0000

    Update deploy script and fix structure

---
+++ src/router/index.js
@@ -0,0 +1,73 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import home from '../pages/home'
+import requirements from '../pages/requirements'
+import research from '../pages/research'
+import hci from '../pages/hci'
+import design from '../pages/design'
+import evaluation from '../pages/evaluation'
+import testing from '../pages/testing'
+import guide from '../pages/guide'
+Vue.use(Router);
+
+export default new Router({
+    scrollBehavior (to, from, savedPosition) {
+        document.querySelector(".navbar-toggler").setAttribute("aria-expanded", "false");
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                if (savedPosition) {
+                    resolve(savedPosition)
+                } else {
+                    resolve({ x: 0, y: 0 })
+                }
+            }, 200)
+        })
+    },
+    linkActiveClass: "active",
+    routes:[
+        {
+            path: "/",
+            component: home,
+            name:"home"
+        },
+        {
+            path: "/requirements",
+            component: requirements,
+            name:"requirements"
+        },
+        {
+            path: "/research",
+            component: research,
+            name:"research"
+        },
+        {
+            path: "/hci",
+            component: hci,
+            name:"hci"
+        },
+        {
+            path: "/design",
+            component: design,
+            name:"design"
+        },
+        {
+            path: "/testing",
+            component: testing,
+            name:"testing"
+        },
+        {
+            path: "/evaluation",
+            component: evaluation,
+            name:"evaluation"
+        },
+        {
+            path: "/guide",
+            component: guide,
+            name:"guide"
+        },
+        {
+            path: "/index.html",
+            redirect: "/"
+        }
+    ]
+})
